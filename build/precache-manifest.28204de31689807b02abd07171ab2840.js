22744b7707d8d8d46ad5a0e0cc898c7d2edc2927
cnqinglin <2207328000@qq.com>
2019-10-26 08:31:34 +0800

    build

---
+++ build/precache-manifest.28204de31689807b02abd07171ab2840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0cd7278b418b479898a381678fd5ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baacf19eb082d0dab020",
+    "url": "/static/css/main.fd31a2e5.chunk.css"
+  },
+  {
+    "revision": "02ae8f11855f841098d8",
+    "url": "/static/js/2.4a72cf2d.chunk.js"
+  },
+  {
+    "revision": "baacf19eb082d0dab020",
+    "url": "/static/js/main.6147b04c.chunk.js"
+  },
+  {
+    "revision": "a977aa85e4c9fe746552",
+    "url": "/static/js/runtime-main.fe5e9d69.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
