e8092dd4674966c3721361294fc48f1fef14d66d
cnqinglin <2207328000@qq.com>
2019-10-26 08:57:46 +0800

    第四次build，纠正错误

---
+++ build/precache-manifest.7590b849402ea9e99f6866045ef7375f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47ee158c9842416ae64f14afccb9224",
+    "url": "/React-Todo-List/build/index.html"
+  },
+  {
+    "revision": "987148f91d7b69872983",
+    "url": "/React-Todo-List/build/static/css/main.fd31a2e5.chunk.css"
+  },
+  {
+    "revision": "02ae8f11855f841098d8",
+    "url": "/React-Todo-List/build/static/js/2.4a72cf2d.chunk.js"
+  },
+  {
+    "revision": "987148f91d7b69872983",
+    "url": "/React-Todo-List/build/static/js/main.764c890b.chunk.js"
+  },
+  {
+    "revision": "7b6d3bfb187e5d12db6a",
+    "url": "/React-Todo-List/build/static/js/runtime-main.5da4439b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/React-Todo-List/build/static/media/logo.25bf045c.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/build/precache-manifest.698c71cf80bfdfbd1adc4b1099f6ab98.js"
+  "/React-Todo-List/build/precache-manifest.7590b849402ea9e99f6866045ef7375f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Todo-List/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
